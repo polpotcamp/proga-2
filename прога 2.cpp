eca288d30a2e7af1a0a8ff9f88e700cc8c439ac7
polpotcamp <85284110+polpotcamp@users.noreply.github.com>
2021-06-03 18:43:03 +0300

    Add files via upload

---
+++ прога 2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int a = -1;
+	char b;
+	cin >> b;
+	string c;
+	cin >> c;
+	for (int i = 0; i < c.length(); i++) {
+		if (c[i] == b) {
+			a = i;
+			break;
+		}
+	}
+	if (a >= 0) {
+		cout << "Символ под номером:" << a + 1;
+	}
+	else {
+		cout << "Нет символа";
+	}
+	return 0;
+}
